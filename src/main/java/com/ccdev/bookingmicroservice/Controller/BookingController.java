92a2bd4d4342f4e27dad53fed1ce6f1f8f6bc291
CrisCanonDev <criscanon21@gmail.com>
2024-06-22 21:10:25 +0200

    Stating if product is in stock via feign call to stock microservice

--- src/main/java/com/ccdev/bookingmicroservice/Controller/BookingController.java
+++ src/main/java/com/ccdev/bookingmicroservice/Controller/BookingController.java
@@ -1,10 +1,12 @@
 package com.ccdev.bookingmicroservice.Controller;
 
+import com.ccdev.bookingmicroservice.Client.StockClient;
 import com.ccdev.bookingmicroservice.Repository.OrderRepository;
 import com.ccdev.bookingmicroservice.dto.OrderDto;
 import com.ccdev.bookingmicroservice.entity.OrderEntity;
 import org.hibernate.query.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +19,24 @@ public class BookingController {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private StockClient stockClient;
+
 
     public String SaveOrder(@RequestBody OrderDto orderDto) {
-        OrderEntity order = new OrderEntity();
-        order.setOrder(UUID.randomUUID().toString());
-        order.setOrderItems(orderDto.getOrderItems());
 
-        orderRepository.save(order);
+        boolean inStock = orderDto.getOrderItems().stream().allMatch(orderItem -> stockClient.isStocked(orderItem.getCode()));
 
-        return"order saved";
-}
+        if(inStock){
+            OrderEntity order = new OrderEntity();
+            order.setOrder(UUID.randomUUID().toString());
+            order.setOrderItems(orderDto.getOrderItems());
+
+            orderRepository.save(order);
+
+            return"order saved";
+        }
+
+        return "order cannot be saved";
+    }
 }
